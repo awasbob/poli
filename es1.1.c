b7ac6dea4e6af52856cb1267a8f908ba6175d5ee
awasbob <alessandro.giargia1234@gmail.com>
2024-10-11 00:39:13 +0200

    Update es1.1.c

--- es1.1.c
+++ es1.1.c
@@ -1,25 +1,50 @@
+/*
+es 1.1 DA CONSEGNARE
+*/
+
+/*
++ attualmente funzionante con stringhe nel main preimpostate
+- quando si incrementa regexp controllare che non sia alla fine della stringa -> es. abcd\a darebbe errore
+- aggiornare tutti i casi di FAIL come il primo
+- sistemare main (prendere src da input)
+- aggiungere stampa finale (tutta la parola non solo la lettera)
+- FACOLTATIVO aggiungere controlli 
+*/
+
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 
+#define CMAX 100
+
 char *cercaRegexp(char *src, char *regexp);
 
 int main() {
-    // MAIN
-    
+    char *src, *regexp; 
+    char * p; 
+
+    src="ciao cici"; 
+    regexp="c.ci"; 
+    /*
+    printf("Inserisci stringa src: \n"); 
+    scanf("%[^\n]", src); // deve leggere più parole
+    printf("Inserisci stringa regexp: \n"); 
+    scanf("%s\n", regexp); 
+    */
+    if ((p = cercaRegexp(src, regexp)) == NULL) printf("Non trovato"); 
+    else printf("Primo carattere: %c", *p);  
+
     return 0; 
 }
 
-// nonrec
-
 char *cercaRegexp(char *src, char *regexp) {
-    int lens, lenr, lenr_in, cont=0; 
+    int lens, lenr, lenr_in, cont=0, flag=0; 
     char * regexp_in = regexp, * src_ret = src; 
     lens = strlen(src);
     lenr = strlen(regexp); 
     lenr_in=lenr;
 
-    while(lenr>0 && lens>0) { 
+    while(lenr>1 && lens>1) { 
         lenr = strlen(regexp);
         lens = strlen(src);
 
@@ -29,9 +54,14 @@ char *cercaRegexp(char *src, char *regexp) {
             cont++; 
         }
         else{
-            if(isalpha(*regexp)!=0) {
+            if(isalpha(*regexp)!=0) { // FAIL
                 regexp = regexp_in;
-                src++;          // MODIFICABILE: portare src al prossimo carattere dopo spazio vuoto invece che al carattere successivo
+                for (; *src != '\0'; src++) {
+                    if (*src == ' ') {
+                        src++;  // Salta lo spazio
+                        break;
+                    }
+                }
                 src_ret=src;
                 cont=0; 
             }
@@ -47,30 +77,45 @@ char *cercaRegexp(char *src, char *regexp) {
                         regexp++; 
                         if(*regexp=='^'){
                             regexp++; 
-                            while(*regexp!=']'){  //MODIFICABILE: in regexp dentro a [^...] c'è sempre solo una lettera
-                                if(*regexp==*src) regexp++; 
-                                else{
-                                    src++; 
-                                    cont++; 
-                                    while(*regexp!=']') regexp++; 
-                                    regexp++;
-                                }
+                            if(*regexp!=*src) { 
+                                cont++; 
+                                regexp++; // punta a ']'
+                                regexp++;
+                                src++; 
+                            }
+                            else{ // FAIL
+                                for(int i=0;i<lens && *src!=' ';i++, src++); 
+                                src++;
+                                src_ret=src;
+                                regexp = regexp_in; 
+                                cont=0;
                             }
                         }
                         else {
-                            while(*regexp!=']'){  // serve aggiungere un flag di controllo all'uscita del while
+                            while(*regexp!=']'){ 
                                 if(*regexp!=*src) regexp++; 
                                 else{
                                     src++; 
                                     cont++; 
-                                    while(*regexp!=']') regexp++; 
-                                    regexp++;
+                                    while(*regexp!=']'){
+                                        regexp++;
+                                        flag=1; 
+                                    }  
                                 }
                             }
+                            regexp++;
+                            if(flag==0) { // FAIL
+                                for(int i=0;i<lens && *src!=' ';i++, src++); 
+                                src++;
+                                src_ret=src;
+                                regexp = regexp_in; 
+                                cont=0;
+                            }
+                            flag=0;
                         } 
                         break;
 
-                    case '\\': //stesso modo di prima, porta avanti i puntatori, controlla con a islower 
+                    case '\\': 
                         regexp++; 
                         if(*regexp=='a') { 
                             if(iswlower(*src)!=0) {
@@ -78,9 +123,11 @@ char *cercaRegexp(char *src, char *regexp) {
                                 regexp++;
                                 cont++; 
                             }
-                            else {
+                            else { // FAIL
+                                for(int i=0;i<lens && *src!=' ';i++, src++); 
+                                src++;
+                                src_ret=src;
                                 regexp = regexp_in; 
-                                src++; // MODIFICABILE come sopra src fino a dopo ' '
                                 cont=0;
                             }
                         }
@@ -90,19 +137,20 @@ char *cercaRegexp(char *src, char *regexp) {
                                 regexp++;
                                 cont++; 
                             }
-                            else{
+                            else{ // FAIL
+                                for(int i=0;i<lens && *src!=' ';i++, src++); 
+                                src++;
+                                src_ret=src;
                                 regexp = regexp_in; 
-                                src++; // MODIFICABILE come sopra src fino a dopo ' '
                                 cont=0;
                             }
-                            }
-                        break; 
-                    
+                        }
+                        break;                     
                 }
             }
         } 
     }
 
-    if(lenr==0 && cont==lenr_in)  return (char*)&src_ret; 
+    if(lenr==1 && cont==lenr_in)  return (char*)&src_ret[0]; //function return address of local variable
     return NULL; 
 }
